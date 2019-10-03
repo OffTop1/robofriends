0feddde90c1d536e709010133a9d9f865844754b
OffTop1 <1quyoshbekm@gmail.com>
2019-10-03 11:51:17 +0300

    Updates

---
+++ precache-manifest.bdfe2cc0849b6609f4f99f91c7f77456.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dbce8bab79f24965ed85f4c225b4501",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "527dbf1bac6442fd84d9",
+    "url": "/robofriends/static/css/2.07548e1c.chunk.css"
+  },
+  {
+    "revision": "76266f06f4fdd469dfd5",
+    "url": "/robofriends/static/css/main.52f79daa.chunk.css"
+  },
+  {
+    "revision": "527dbf1bac6442fd84d9",
+    "url": "/robofriends/static/js/2.916945fa.chunk.js"
+  },
+  {
+    "revision": "76266f06f4fdd469dfd5",
+    "url": "/robofriends/static/js/main.bcf83eda.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
